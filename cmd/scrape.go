2522183e74aa1d72e12fde9c5ee560ec758963b8
ks6088ts <ks6088ts@gmail.com>
2024-07-19 16:04:28 +0900

    implement scrape command

--- cmd/scrape.go
+++ cmd/scrape.go
@@ -22,36 +22,94 @@ THE SOFTWARE.
 package cmd
 
 import (
+	"crypto/md5"
 	"fmt"
+	"log"
+	"os"
+	"path/filepath"
 
+	"github.com/playwright-community/playwright-go"
 	"github.com/spf13/cobra"
 )
+
+func assertErrorToNilf(message string, err error) {
+	if err != nil {
+		log.Fatalf(message, err)
+	}
+}
 
 // scrapeCmd represents the scrape command
 var scrapeCmd = &cobra.Command{
 	Use:   "scrape",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Scrape urls",
+	Long:  `Scrape urls`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("scrape called")
+		// Parse flags
+		urls, err := cmd.Flags().GetStringArray("url")
+		assertErrorToNilf("failed to parse `url`: %w", err)
+		dir, err := cmd.Flags().GetString("dir")
+		assertErrorToNilf("failed to parse `dir`: %w", err)
+		headless, err := cmd.Flags().GetBool("headless")
+		assertErrorToNilf("failed to parse `headless`: %w", err)
+
+		// Create output directory
+		cwd, err := os.Getwd()
+		assertErrorToNilf("could not get cwd: %w", err)
+		err = os.MkdirAll(filepath.Join(cwd, dir), os.ModePerm)
+		assertErrorToNilf("could not create output directory: %w", err)
+
+		// Scrape via Playwright
+		pw, err := playwright.Run()
+		assertErrorToNilf("could not launch playwright: %w", err)
+		browser, err := pw.Chromium.Launch(playwright.BrowserTypeLaunchOptions{
+			Headless: playwright.Bool(headless),
+		})
+		assertErrorToNilf("could not launch Chromium: %w", err)
+		context, err := browser.NewContext()
+		assertErrorToNilf("could not create context: %w", err)
+		page, err := context.NewPage()
+		assertErrorToNilf("could not create page: %w", err)
+
+		// TODO: parallelize
+		for _, url := range urls {
+			fmt.Printf("Scraping %s\n", url)
+			_, err := page.Goto(url, playwright.PageGotoOptions{
+				WaitUntil: playwright.WaitUntilStateDomcontentloaded,
+			})
+			assertErrorToNilf("could not goto: %w", err)
+
+			fileName, err := getFileName(url)
+			assertErrorToNilf("could not get file name: %w", err)
+
+			_, err = page.Screenshot(playwright.PageScreenshotOptions{
+				Path: playwright.String(filepath.Join(cwd, dir, fileName)),
+			})
+			assertErrorToNilf("could not take screenshot: %w", err)
+		}
+
+		// Close browser
+		err = browser.Close()
+		assertErrorToNilf("could not close browser: %w", err)
+		err = pw.Stop()
+		assertErrorToNilf("could not stop playwright: %w", err)
 	},
+}
+
+func getFileName(url string) (string, error) {
+	md5 := md5.New()
+	_, err := md5.Write([]byte(url))
+	if err != nil {
+		return "", fmt.Errorf("could not write md5: %w", err)
+	}
+	return fmt.Sprintf("%x.png", md5.Sum(nil)), nil
 }
 
 func init() {
 	rootCmd.AddCommand(scrapeCmd)
 
-	// Here you will define your flags and configuration settings.
+	scrapeCmd.Flags().StringArrayP("url", "u", []string{}, "URL to scrape")
+	scrapeCmd.Flags().StringP("dir", "d", "artifacts", "Output directory")
+	scrapeCmd.Flags().BoolP("headless", "m", true, "Headless mode")
 
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// scrapeCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// scrapeCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	assertErrorToNilf("could not mark `url` as required: %w", scrapeCmd.MarkFlagRequired("url"))
 }
