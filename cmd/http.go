c28f5251d602488d45d2fb4b8bd6721ac4067b1b
ks6088ts <ks6088ts@gmail.com>
2024-07-20 16:06:43 +0900

    implement simple http server

---
+++ cmd/http.go
@@ -0,0 +1,64 @@
+/*
+Copyright © 2024 ks6088ts
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+package cmd
+
+import (
+	"io"
+	"log"
+	"math/rand"
+	"net/http"
+	"strconv"
+
+	"github.com/spf13/cobra"
+)
+
+func rolldice(w http.ResponseWriter, r *http.Request) {
+	roll := 1 + rand.Intn(6)
+
+	resp := strconv.Itoa(roll) + "\n"
+	if _, err := io.WriteString(w, resp); err != nil {
+		log.Printf("Write failed: %v\n", err)
+	}
+}
+
+// httpCmd represents the http command
+var httpCmd = &cobra.Command{
+	Use:   "http",
+	Short: "Start a HTTP server",
+	Long:  `Start a HTTP server that listens on the specified port.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		port, err := cmd.Flags().GetInt("port")
+		if err != nil {
+			log.Printf("unable to parse `port`: %v", port)
+		}
+
+		http.HandleFunc("/rolldice", rolldice)
+
+		log.Fatal(http.ListenAndServe(":"+strconv.Itoa(port), nil))
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(httpCmd)
+
+	httpCmd.Flags().IntP("port", "p", 8080, "Port number")
+}
