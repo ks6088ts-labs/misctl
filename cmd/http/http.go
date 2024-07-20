f6041fd5999be7876c98ef070a805d5292d9a1f6
ks6088ts <ks6088ts@gmail.com>
2024-07-20 16:09:28 +0900

    create http package

--- cmd/http/http.go
+++ cmd/http/http.go
@@ -19,7 +19,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
-package cmd
+package http
 
 import (
 	"io"
@@ -58,7 +58,9 @@ var httpCmd = &cobra.Command{
 }
 
 func init() {
-	rootCmd.AddCommand(httpCmd)
-
 	httpCmd.Flags().IntP("port", "p", 8080, "Port number")
+}
+
+func GetCommand() *cobra.Command {
+	return httpCmd
 }
